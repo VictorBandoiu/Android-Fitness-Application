c6e32417c1aff1abc2e976040f9bf182ca47064c
VictorBandoiu <victorbandoiu@yahoo.com>
2015-11-27 10:02:54 +0100

    add click button3 for options

--- app/src/main/java/com/example/francois/android_fitness_application/MainActivity.java
+++ app/src/main/java/com/example/francois/android_fitness_application/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.francois.android_fitness_application;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.button3:
+                Intent i = new Intent(this, OptionsActivity.class);
+                startActivityForResult(i, 1);
+                break;
+
+    }
+}
 }
