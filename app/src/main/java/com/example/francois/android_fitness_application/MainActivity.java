90f017b632c38d7901f068100a8e71478ad865ca
francois <furtfight@yahoo.fr>
2015-11-27 10:17:30 +0100

    dummy change to test pull request and merge

--- app/src/main/java/com/example/francois/android_fitness_application/MainActivity.java
+++ app/src/main/java/com/example/francois/android_fitness_application/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+//dummy change
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
