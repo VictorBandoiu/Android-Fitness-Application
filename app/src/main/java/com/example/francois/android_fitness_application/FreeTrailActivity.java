88c279771b1c7ed9644ab599c8b08f038dda952c
francois <furtfight@yahoo.fr>
2015-12-09 16:59:51 +0100

    connection of free trail and predefined route button

--- app/src/main/java/com/example/francois/android_fitness_application/FreeTrailActivity.java
+++ app/src/main/java/com/example/francois/android_fitness_application/FreeTrailActivity.java
@@ -1,11 +1,21 @@
 package com.example.francois.android_fitness_application;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import com.google.android.gms.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.text.style.UpdateAppearance;
+import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,9 +23,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FreeTrailActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.text.DateFormat;
+import java.util.Date;
+
+public class FreeTrailActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    Boolean run = false;
+    Path path;
+
+    Location mLastLocation;
+    Location mCurrentLocation;
+    Date mLastUpdateTime;
+    Date mUpdateTime;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +47,52 @@ public class FreeTrailActivity extends FragmentActivity implements OnMapReadyCal
         setContentView(R.layout.activity_free_trail);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
+        textView= (TextView)findViewById(R.id.textView2);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        createLocationRequest();
+        createGoogleApiClient();
     }
 
+    public void createGoogleApiClient(){
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void StartRun(){
+        run = true;
+        path = new Path();
+    }
+
+    protected  void StopRun(){
+        run =false;
+        textView.setText(Float.toString(path.distance())+" meters done \n"+Integer.toString(path.numberOfPoints())+" points of location");
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button4:
+                StartRun();
+                break;
+            case R.id.button5:
+                StopRun();
+                break;
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -54,4 +119,56 @@ public class FreeTrailActivity extends FragmentActivity implements OnMapReadyCal
             // Show rationale and request permission.
         }
     }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        startLocationUpdates();
+
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = mCurrentLocation;
+        mCurrentLocation = location;
+        mLastUpdateTime = mUpdateTime;
+        mUpdateTime = new Date();
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(sydney).title("your position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (run){
+            path.newLocation(mCurrentLocation, mUpdateTime);
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putParcelable("LOCATION", mCurrentLocation);
+        //savedInstanceState.putString("TIME", mUpdateTime);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
 }
---
+++ app/src/main/java/com/example/francois/android_fitness_application/Path.java
@@ -0,0 +1,69 @@
+package com.example.francois.android_fitness_application;
+
+import android.location.Location;
+
+import java.util.Date;
+import java.util.LinkedList;
+
+/**
+ * Created by francois on 09-12-15.
+ */
+public class Path {
+    LinkedList<PathData> locations;
+
+    public  Path(){
+        locations = new LinkedList();
+    }
+    public void newLocation(Location location, Date timeStamp){
+        locations.add(new PathData(location,timeStamp));
+    }
+
+    public  float speed(){
+        return distance()/(duration()*1000);
+    }
+    public float distance(){
+        float distance = 0;
+        Location start = locations.getFirst().location;
+        Location stop = locations.get(1).location;
+        for (int i=2; i<locations.size(); i++){
+            distance =+ deltaTwoPoints(start,stop);
+            start = stop;
+            stop = locations.get(i).location;
+        }
+        return distance;
+    }
+
+    //TODO delete
+
+    public int numberOfPoints(){
+        return locations.size();
+    }
+    public float duration(){
+        float duration = 0;
+        Date start = locations.getFirst().timestamp;
+        Date stop = locations.get(1).timestamp;
+        for (int i=2; i<locations.size(); i++){
+            duration =+ deltaTwoDates(start,stop);
+            start = stop;
+            stop = locations.get(i).timestamp;
+        }
+        return duration;
+    }
+    private float deltaTwoPoints(Location start, Location stop){
+        return start.distanceTo(stop);
+    }
+
+    private long deltaTwoDates(Date start, Date stop){
+        return stop.getTime() - start.getTime();
+    }
+}
+
+class PathData{
+    Location location;
+    Date timestamp;
+
+    public PathData(Location location, Date timestamp){
+        this.location = location;
+        this.timestamp = timestamp;
+    }
+}
